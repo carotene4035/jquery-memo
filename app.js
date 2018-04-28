088cceeedeefd801f4f866c24ea8a2c20f5440b1
carotene512 <karoten512@gmail.com>
2018-04-28 12:11:23 +0900

    メモ帳としての最低限の機能が実装できた。

--- app.js
+++ app.js
@@ -12,8 +12,25 @@
    * Memo class
    */
   function Memo(element) {
+    this.element = element;
     this.id = $(element).data('id');
     this.content = $(element).text();
+  }
+
+  Memo.prototype.updateMemo = function(content) {
+    /** viewの更新 */
+    $(this.element).text(content);
+    /** modelの更新 */
+    this.content = content;
+    console.log(this);
+  }
+
+  Memo.prototype.setActive = function() {
+    $(this.element).addClass('active');
+  }
+
+  Memo.prototype.setNonActive = function() {
+    $(this.element).removeClass('active');
   }
 
   /*
@@ -21,13 +38,27 @@
    */
   function WorkSpace(element) {
     /** memo情報を保持 */
-    this.id;
-    this.memo;
+    this.element = element;
+    this.currentMemo;
   };
 
-  /** 現在編集中のメモのidを渡す */
-  WorkSpace.prototype.setMemoId = function(memoId) {
-    this.id = memoId;
+  /** 現在編集中のメモを渡す */
+  WorkSpace.prototype.setCurrentMemo = function(memoObj) {
+    if (this.currentMemo !== undefined) {
+      this.currentMemo.setNonActive();
+    }
+    this.currentMemo = memoObj;
+    this._render(this.currentMemo.content);
+  }
+
+  WorkSpace.prototype._render = function(content) {
+    $(this.element).val(content);
+  }
+
+  WorkSpace.prototype.updateContent = function() {
+    var str = $(this.element).val();
+    /** memoを更新 */
+    this.currentMemo.updateMemo(str);
   }
 
   /** 初期化 */
@@ -43,33 +74,22 @@
     });
   });
 
-//  /** workspaceのclickイベントの実装 */
-//  $('.workspace').on('keyup', function() {
-//    /** 更新が起こるたびに再描画 */
-//    console.log(
-//      $(this).data('data.workspace')
-//    );
-//  });
-//
-  /** memo cardのclickイベントの実装 */
+  /** workspaceのclickイベントの実装 */
+  $('.workspace').on('keyup', function() {
+    var wo =  $(this).data('data.workspace')
+    wo.updateContent();
+  });
+
+
   $('.element').on('click', function() {
     upDateWorkSpace.call(this);
   });
 
-
   function upDateWorkSpace() {
-    /*
-     * 1. memoのidを取得
-     * 2. workspaceのobjectを取得
-     * 3. memoのidをset
-     */
-    // 1
-    var memoId = $(this).data('id')
-    var memo = $(this).text()
-    // 2
+    var memoObj = $(this).data('data.memo')
+    memoObj.setActive();
     var wo = $('.workspace').data('data.workspace');
-    wo.setMemoId(memoId);
-    $('.workspace').val(memo);
+    wo.setCurrentMemo(memoObj);
   }
 
 })();
